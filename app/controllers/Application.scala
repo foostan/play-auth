b1e763ba9db5c702b4e3c6be8e176939d92cec20
foostan <ks@fstn.jp>
2015-02-08 23:33:42 +0900

    Remove views

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,14 +8,12 @@ import util.OAuth2
 
 object Application extends Controller {
 
-  def index = Action { implicit request =>
+  def authorization = Action { implicit request =>
     val oauth2 = new OAuth2(Play.current)
     val callbackUrl = util.routes.OAuth2.callback(None, None).absoluteURL()
     val scope = "repo"   // github scope - request repo access
     val state = UUID.randomUUID().toString  // random confirmation string
     val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, scope, state)
-    Ok(views.html.index("Your new application is ready.", redirectUrl)).
-      withSession("oauth-state" -> state)
+    Redirect(redirectUrl).withSession("oauth-state" -> state)
   }
-
 }
